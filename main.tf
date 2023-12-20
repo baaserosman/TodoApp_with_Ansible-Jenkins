ed9c04c132ff95736bedc6e00cb7bd318dc689c0
baaserosman <baaser.osman@gmail.com>
2023-12-20 16:34:34 +0100

    main.tf updated

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ resource "aws_instance" "managed_nodes" {
   tags = {
     Name = "ansible_${element(var.tags, count.index )}"
     stack = "ansible_project"
-    # environment = "development"
+    environment = "development"
   }
   user_data = <<-EOF
             #! /bin/bash
